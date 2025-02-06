f8c63a2e030a86005e811ef732ccdc18e47ef433
kabanosk <56364007+Kabanosk@users.noreply.github.com>
2025-02-06 21:04:13 +0100

    Add endpoint for recomendations

--- src/controller/recommendations.py
+++ src/controller/recommendations.py
@@ -1,13 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Form
 from fastapi.templating import Jinja2Templates
+
+from recommender.recommender import RecommenderSystem
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="src/view")
+recommender = RecommenderSystem(
+    data_folder="data/embeddings", model_name="snowflake-arctic-embed"
+)
 
 @router.get("/user")
 async def get_user_recommendations():
     return {"message": "User recommendations"}
 
-@router.get("/movie/{movie_id}")
-async def get_movie_recommendations(movie_id: int):
-    return {"message": f"Recommendations for movie {movie_id}"}
+@router.post("/movie")
+async def get_movie_recommendations(
+    title: str = Form(...),
+    description: str = Form(...),
+    genre: str = Form(...)
+):
+    try:
+        print(len(recommender.item_embeddings))
+        recommendations = recommender.recommend_similar_items({"title": title, "description": description, "genre": genre})
+        return {"recommendations": recommendations}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
